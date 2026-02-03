4e2f655edac931821cdb09c295e43557944c3cfe
Max <max.lindqvist@ipp.mpg.de>
2026-02-03 15:18:27 +0100

    Improve reqs, docs and CI (#13)
    * Updated pre-commit hooks
    
    * Updated pre commit hooks
    
    * isort before black
    
    * Updated python versions
    
    * Moved tests into the src
    
    * Added script for setting up project
    
    * Rename the import
    
    * Updated prints
    
    * Added more deps
    
    * Added _static
    
    * Moved custom.css
    
    * Rewrote the docs a bit
    
    * Updated the docs
    
    * Autosummary to gitignore
    
    * Added ruff to pre-commit
    
    * Added test_docs_job
    
    * Adde test_docs
    
    * isort,black --> ruff
    
    * Added rm
    
    * Test setup script
    
    * Fixes
    
    * Renaming
    
    * Separate tutorial and pytest
    
    * Fixed setup_project.sh
    
    * Test
    
    * Fixed setup_project.sh
    
    * Remove the test file in the script
    
    * Run everything relative to the script directory
    
    * Fixed docs build
    
    * Removed .github/workflows/test.yml
    
    * copy tutorials
    
    * Fix publishing to pypi
    
    * Formatting with prettier

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,26 +2,21 @@
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
-
 import os
 import shutil
+import sys
+from pathlib import Path
 
+sys.path.insert(0, os.path.abspath("../src"))
 
-def copy_tutorials(app):
-    src = os.path.abspath("../tutorials")
-    dst = os.path.abspath("source/tutorials")
+# Copy tutorial notebooks from project root to docs/source/tutorials/
+tutorials_source = Path(__file__).parent.parent.parent / "tutorials"
+tutorials_dest = Path(__file__).parent / "tutorials"
 
-    # Remove existing target directory if it exists
-    if os.path.exists(dst):
-        shutil.rmtree(dst)
-
-    shutil.copytree(src, dst)
-
-
-def setup(app):
-    app.connect("builder-inited", copy_tutorials)
-    # app.add_stylesheet("my-styles.css")
-    app.add_css_file("custom.css")
+if tutorials_source.exists():
+    tutorials_dest.mkdir(exist_ok=True)
+    for notebook in tutorials_source.glob("*.ipynb"):
+        shutil.copy2(notebook, tutorials_dest / notebook.name)
 
 
 # -- Project information -----------------------------------------------------
@@ -32,46 +27,75 @@ copyright = "2025, Max"
 author = "Max"
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
 extensions = [
-    "nbsphinx",
-    "sphinx.ext.mathjax",
     "sphinx.ext.autodoc",
-    "myst_parser",  # enable Markdown support
+    "sphinx.ext.autosummary",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
+    "sphinxcontrib.mermaid",
+    "sphinx_design",
+    "nbsphinx",
 ]
 
-exclude_patterns = []
-
-# Recognize both .rst and .md
-source_suffix = {
-    ".rst": "restructuredtext",
-    ".md": "markdown",
-}
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-# Activate the theme.
-html_theme = "sphinx_book_theme"
-
-
-html_static_path = ["_static"]
+# Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "**/.ipynb_checkpoints"]
 
-# html_sidebars = {"**": []}
+# nbsphinx configuration
+nbsphinx_execute = "never"  # Don't execute notebooks during build
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = "sphinx_book_theme"
 
 html_theme_options = {
     "repository_branch": "devel",
-    "show_toc_level": 3,
+    "show_toc_level": 2,
     "secondary_sidebar_items": ["page-toc"],
+    "use_sidenotes": True,
+    "home_page_in_toc": True,
+    "collapse_navigation": False,
+    "navigation_depth": 2,
     "icon_links": [
         {
             "name": "GitHub",
-            "url": "https://github.com/max-models/template-python",
+            "url": "https://github.com/max-models/template-projects/",
             "icon": "fab fa-github",
             "type": "fontawesome",
         },
+        {
+            "name": "⭐ Star us!",
+            "url": "https://github.com/max-models/template-projects/",
+            "icon": "fas fa-star",
+            "type": "fontawesome",
+        },
     ],
+}
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+breathe_separate_member_pages = False
+html_theme = "sphinx_book_theme"
+html_static_path = ["_static"]
+html_css_files = ["custom.css"]
+
+autosummary_generate = True
+
+autodoc_default_options = {
+    "members": True,
+    "undoc-members": False,
+    "show-inheritance": True,
 }
